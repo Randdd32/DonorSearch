2a39fd28683680bc84cdcabf63634f15f39efc3d
Илья <@WIN-RANNDDD>
2026-02-02 18:20:50 +0400

    Project initialization for backend

---
+++ backend/src/main/java/com/github/randdd32/donor_search_backend/DonorSearchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.randdd32.donor_search_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonorSearchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonorSearchBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/randdd32/donor_search_backend/DonorSearchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.randdd32.donor_search_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonorSearchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
