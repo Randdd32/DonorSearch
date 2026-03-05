1925ec02615d6f28e14fa63c86e996e83289766d
Илья <@WIN-RANNDDD>
2026-03-05 13:58:14 +0400

    Add files for case, cpu, cpu cooler, motherboard, memory, videocard, power supply, case fan, monitor

--- knowledge-base/pc-part-importer/app/utils/constants.py
+++ knowledge-base/pc-part-importer/app/utils/constants.py
@@ -1,13 +1,22 @@
 import os
 
-from app.importers.optical_drive_importer import OpticalDriveImporter
+from app import importers
 
 DB_URL = os.getenv("DB_URL", "postgresql://postgres:postgres@localhost:5432/donor_search_db")
 
 DATA_DIR = os.getenv("DATA_DIR", "./data")
 
 IMPORTER_MAP = {
-    'optical-drive': OpticalDriveImporter,
+    'optical-drive': importers.OpticalDriveImporter,
+    'case': importers.CaseImporter,
+    'cpu': importers.CPUImporter,
+    'cpu-cooler': importers.CPUCoolerImporter,
+    'motherboard': importers.MotherboardImporter,
+    'memory': importers.MemoryImporter,
+    'video-card': importers.VideoCardImporter,
+    'power-supply': importers.PowerSupplyImporter,
+    'case-fan': importers.CaseFanImporter,
+    'monitor': importers.MonitorImporter
 }
 
 ALL_PART_TYPES = sorted(list(IMPORTER_MAP.keys()))
