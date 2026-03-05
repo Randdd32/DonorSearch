467fd121c56c313cab5bc06363fa239e926b74d9
Илья <@WIN-RANNDDD>
2026-03-05 10:54:03 +0400

    Replace similarity() DESC with Postgres KNN distance operator <-> in OpticalDriveRepository

--- backend/src/main/java/com/github/randdd32/donor_search_backend/repository/hardware/OpticalDriveRepository.java
+++ backend/src/main/java/com/github/randdd32/donor_search_backend/repository/hardware/OpticalDriveRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 public interface OpticalDriveRepository extends HardwareRepository<OpticalDriveEntity> {
     @Override
     @Query(value = "SELECT * FROM component_optical_drive " +
-            "ORDER BY similarity(search_name, :searchToken) DESC LIMIT 1",
+            "ORDER BY search_name <-> :searchToken ASC LIMIT 1",
             nativeQuery = true)
     Optional<OpticalDriveEntity> findMostSimilar(@Param("searchToken") String searchToken);
 }
