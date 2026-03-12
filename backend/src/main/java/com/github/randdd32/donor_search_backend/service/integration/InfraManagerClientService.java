9baa0eef2957908dac3819a065d389add19abeba
Илья <@WIN-RANNDDD>
2026-03-12 09:52:05 +0400

    Correct indentation

--- backend/src/main/java/com/github/randdd32/donor_search_backend/service/integration/InfraManagerClientService.java
+++ backend/src/main/java/com/github/randdd32/donor_search_backend/service/integration/InfraManagerClientService.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@ Service
+@Service
 @RequiredArgsConstructor
 public class InfraManagerClientService {
     private final NamedParameterJdbcTemplate msSqlJdbcTemplate;
