666903bee0544cbbff1533457843f737562d0f78
Илья <@WIN-RANNDDD>
2026-03-17 22:34:57 +0400

    Fix foreign key reference for optical drive form factor in OpticalDrive model

--- knowledge-base/pc-part-importer/app/database.py
+++ knowledge-base/pc-part-importer/app/database.py
@@ -3,6 +3,18 @@ from sqlalchemy.orm import sessionmaker
 from app.models.base import Base
 from app.utils.constants import DB_URL
 
+import app.models.dictionaries
+import app.models.component
+import app.models.optical_drive
+import app.models.video_card
+import app.models.case
+import app.models.power_supply
+import app.models.case_fan
+import app.models.cpu
+import app.models.cpu_cooler
+import app.models.storage
+import app.models.expansion_card
+
 engine = create_engine(DB_URL, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- knowledge-base/pc-part-importer/app/models/optical_drive.py
+++ knowledge-base/pc-part-importer/app/models/optical_drive.py
@@ -6,7 +6,7 @@ class OpticalDrive(Component):
     __tablename__ = "component_optical_drive"
     id: Mapped[int] = mapped_column(ForeignKey("component.id", ondelete="CASCADE"), primary_key=True)
     
-    form_factor_id: Mapped[int | None] = mapped_column(ForeignKey("dic_form_factor_optical_drive.id"))
+    form_factor_id: Mapped[int | None] = mapped_column(ForeignKey("dic_optical_drive_form_factor.id"))
     interface_id: Mapped[int | None] = mapped_column(ForeignKey("dic_storage_interface.id"))
 
     bd_speed_x: Mapped[int | None] = mapped_column(SmallInteger)
